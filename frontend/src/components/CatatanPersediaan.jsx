f19e724494dc33d8524fe78fc6a1a893fa28fa58
Rahmat Ramadhan <rakarr15@gmail.com>
2025-02-21 18:04:26 +0700

    v3.0.2.1
    tabel pkm dan kernel

--- frontend/src/components/CatatanPersediaan.jsx
+++ frontend/src/components/CatatanPersediaan.jsx
@@ -66,14 +66,15 @@ const CatatanPersediaan = () => {
 
   // Filter data berdasarkan tanggal & lokasi
   const filteredData = dataPenyimpanan.filter(
-    (item) => item.lokasi === selectedLocation && item.tanggal.startsWith(selectedDate)
+    (item) =>
+      item.lokasi === selectedLocation && item.tanggal.startsWith(selectedDate)
   );
 
   const filteredDataPrevious = dataPenyimpanan.filter(
-    (item) => item.lokasi === selectedLocation && item.tanggal.startsWith(previousDate)
+    (item) =>
+      item.lokasi === selectedLocation && item.tanggal.startsWith(previousDate)
   );
-  
-  
+
   return (
     <Container>
       <h2 className="text-center mb-4">PERSEDIAAN PRODUKSI CPO & PKO</h2>
@@ -81,7 +82,10 @@ const CatatanPersediaan = () => {
         <Col md={6}>
           <Form.Group controlId="tanggalSelect">
             <Form.Label>Pilih Tanggal:</Form.Label>
-            <Form.Select value={selectedDate} onChange={(e) => setSelectedDate(e.target.value)}>
+            <Form.Select
+              value={selectedDate}
+              onChange={(e) => setSelectedDate(e.target.value)}
+            >
               {availableDates.map((date) => (
                 <option key={date} value={date}>
                   {new Date(date).toLocaleDateString()}
@@ -93,7 +97,10 @@ const CatatanPersediaan = () => {
         <Col md={6}>
           <Form.Group controlId="lokasiSelect">
             <Form.Label>Pilih Lokasi:</Form.Label>
-            <Form.Select value={selectedLocation} onChange={(e) => setSelectedLocation(e.target.value)}>
+            <Form.Select
+              value={selectedLocation}
+              onChange={(e) => setSelectedLocation(e.target.value)}
+            >
               {availableLocations.map((loc) => (
                 <option key={loc} value={loc}>
                   {loc}
@@ -183,34 +190,6 @@ const CatatanPersediaan = () => {
                           </tr>
                         ))}
 
-                        {/* Loop setiap penyimpanan dalam kategori PKM*/}
-                        <tr key={`pkm-${index}-${katIndex}`}>
-                          <td>
-                            <strong>PKM</strong>
-                          </td>
-                          <td>{}</td>
-                          <td>{item.pkm.stok}</td>
-                          <td>{}</td>
-                          <td>{}</td>
-                          <td>{}</td>
-                          <td>{item.pkm.nilai_do}</td>
-                          <td>{item.pkm.nilai_hi}</td>
-                        </tr>
-
-                        {/* Loop setiap penyimpanan dalam kategori Kernel*/}
-                        <tr key={`kernel-${index}-${katIndex}`}>
-                          <td>
-                            <strong>Kernel</strong>
-                          </td>
-                          <td>{}</td>
-                          <td>{item.kernel.stok}</td>
-                          <td>{item.kernel.alb}</td>
-                          <td>{item.kernel.kadar_air}</td>
-                          <td>{item.kernel.kadar_kotoran}</td>
-                          <td>{item.kernel.do}</td>
-                          <td>{item.kernel.hi}</td>
-                        </tr>
-
                         {/* Baris jumlah untuk kategori */}
                         <tr
                           key={`jumlah-${index}-${katIndex}`}
@@ -241,6 +220,31 @@ const CatatanPersediaan = () => {
                         </tr>
                       </>
                     ))}
+
+                    {/* Pindahkan bagian PKM dan Kernel ke bawah setelah kategori */}
+                    <tr key={`pkm-${index}`} className="table-warning">
+                      <td colSpan={2}>
+                        <strong>PKM</strong>
+                      </td>
+                      <td>{item.pkm.stok}</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>{item.pkm.nilai_do}</td>
+                      <td>{item.pkm.nilai_hi}</td>
+                    </tr>
+
+                    <tr key={`kernel-${index}`} className="table-warning">
+                      <td colSpan={2}>
+                        <strong>Kernel</strong>
+                      </td>
+                      <td>{item.kernel.stok}</td>
+                      <td>{item.kernel.alb}</td>
+                      <td>{item.kernel.kadar_air}</td>
+                      <td>{item.kernel.kadar_kotoran}</td>
+                      <td>{item.kernel.do}</td>
+                      <td>{item.kernel.hi}</td>
+                    </tr>
                   </>
                 ))}
               </tbody>
@@ -255,142 +259,139 @@ const CatatanPersediaan = () => {
               : "Sebelumnya"}
           </h4>
           <div className="table-responsive">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th rowSpan={2}>Tanggal</th>
-                <th rowSpan={2}>Lokasi</th>
-                <th rowSpan={2}>Kategori</th>
-                <th rowSpan={2}>Penyimpanan</th>
-                <th rowSpan={2}>Stok</th>
-                <th colSpan={3} className="text-center">
-                  Mutu
-                </th>
-                <th colSpan={2} className="text-center">
-                  DO
-                </th>
-              </tr>
-              <tr>
-                <th>ALB</th>
-                <th>Kadar Air</th>
-                <th>Kadar Kotoran</th>
-                <th>Hi</th>
-                <th>Sd Hi</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredDataPrevious.map((item, index) => (
-                <>
-                  {/* Baris utama untuk PKM dan Kernel */}
-                  <tr key={`main-${index}`}>
-                    <td
-                      rowSpan={Object.keys(item.kategori).reduce(
-                        (acc, key) =>
-                          acc + item.kategori[key].penyimpanan.length + 4,
-                        1
-                      )}
-                    >
-                      {new Date(item.tanggal).toLocaleDateString()}
-                    </td>
-                    <td
-                      rowSpan={Object.keys(item.kategori).reduce(
-                        (acc, key) =>
-                          acc + item.kategori[key].penyimpanan.length + 4,
-                        1
-                      )}
-                    >
-                      {item.lokasi}
-                    </td>
-                  </tr>
-
-                  {/* Loop kategori dan penyimpanan */}
-                  {Object.values(item.kategori).map((kat, katIndex) => (
-                    <>
-                      {/* Baris kategori */}
-                      <tr
-                        key={`kat-${index}-${katIndex}`}
-                        className="table-primary"
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th rowSpan={2}>Tanggal</th>
+                  <th rowSpan={2}>Lokasi</th>
+                  <th rowSpan={2}>Kategori</th>
+                  <th rowSpan={2}>Penyimpanan</th>
+                  <th rowSpan={2}>Stok</th>
+                  <th colSpan={3} className="text-center">
+                    Mutu
+                  </th>
+                  <th colSpan={2} className="text-center">
+                    DO
+                  </th>
+                </tr>
+                <tr>
+                  <th>ALB</th>
+                  <th>Kadar Air</th>
+                  <th>Kadar Kotoran</th>
+                  <th>Hi</th>
+                  <th>Sd Hi</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredDataPrevious.map((item, index) => (
+                  <>
+                    {/* Baris utama untuk tanggal dan lokasi */}
+                    <tr key={`main-${index}`}>
+                      <td
+                        rowSpan={Object.keys(item.kategori).reduce(
+                          (acc, key) =>
+                            acc + item.kategori[key].penyimpanan.length + 3, // Tidak menghitung PKM dan Kernel di rowspan
+                          1
+                        )}
                       >
-                        <td rowSpan={kat.penyimpanan.length + 1}>
-                          <strong>{kat.nama}</strong>
-                        </td>
-                      </tr>
+                        {new Date(item.tanggal).toLocaleDateString()}
+                      </td>
+                      <td
+                        rowSpan={Object.keys(item.kategori).reduce(
+                          (acc, key) =>
+                            acc + item.kategori[key].penyimpanan.length + 3,
+                          1
+                        )}
+                      >
+                        {item.lokasi}
+                      </td>
+                    </tr>
 
-                      {/* Loop setiap penyimpanan nilai dalam kategori */}
-                      {kat.penyimpanan.map((penyimpanan, penyIndex) => (
-                        <tr key={`peny-${index}-${katIndex}-${penyIndex}`}>
-                          <td>{penyimpanan.jenis_tank}</td>
-                          <td>{penyimpanan.stok}</td>
-                          <td>{penyimpanan.alb}</td>
-                          <td>{penyimpanan.kadar_air}</td>
-                          <td>{penyimpanan.kadar_kotoran}</td>
-                          <td>{penyimpanan.do}</td>
-                          <td>{penyimpanan.hi}</td>
+                    {/* Loop kategori dan penyimpanan */}
+                    {Object.values(item.kategori).map((kat, katIndex) => (
+                      <>
+                        {/* Baris kategori */}
+                        <tr
+                          key={`kat-${index}-${katIndex}`}
+                          className="table-primary"
+                        >
+                          <td rowSpan={kat.penyimpanan.length + 1}>
+                            <strong>{kat.nama}</strong>
+                          </td>
                         </tr>
-                      ))}
 
-                      {/* Loop setiap penyimpanan dalam kategori PKM*/}
-                      <tr key={`pkm-${index}-${katIndex}`}>
-                        <td>
-                          <strong>PKM</strong>
-                        </td>
-                        <td>{}</td>
-                        <td>{item.pkm.stok}</td>
-                        <td>{}</td>
-                        <td>{}</td>
-                        <td>{}</td>
-                        <td>{item.pkm.nilai_do}</td>
-                        <td>{item.pkm.nilai_hi}</td>
-                      </tr>
+                        {/* Loop setiap penyimpanan nilai dalam kategori */}
+                        {kat.penyimpanan.map((penyimpanan, penyIndex) => (
+                          <tr key={`peny-${index}-${katIndex}-${penyIndex}`}>
+                            <td>{penyimpanan.jenis_tank}</td>
+                            <td>{penyimpanan.stok}</td>
+                            <td>{penyimpanan.alb}</td>
+                            <td>{penyimpanan.kadar_air}</td>
+                            <td>{penyimpanan.kadar_kotoran}</td>
+                            <td>{penyimpanan.do}</td>
+                            <td>{penyimpanan.hi}</td>
+                          </tr>
+                        ))}
 
-                      {/* Loop setiap penyimpanan dalam kategori Kernel*/}
-                      <tr key={`kernel-${index}-${katIndex}`}>
-                        <td>
-                          <strong>Kernel</strong>
-                        </td>
-                        <td>{}</td>
-                        <td>{item.kernel.stok}</td>
-                        <td>{item.kernel.alb}</td>
-                        <td>{item.kernel.kadar_air}</td>
-                        <td>{item.kernel.kadar_kotoran}</td>
-                        <td>{item.kernel.do}</td>
-                        <td>{item.kernel.hi}</td>
-                      </tr>
+                        {/* Baris jumlah untuk kategori */}
+                        <tr
+                          key={`jumlah-${index}-${katIndex}`}
+                          className="table-secondary"
+                        >
+                          <td>
+                            <strong>Total {kat.nama}</strong>
+                          </td>
+                          <td></td>
+                          <td>
+                            <strong>{kat.jumlah.stok}</strong>
+                          </td>
+                          <td>
+                            <strong>{kat.jumlah.alb}</strong>
+                          </td>
+                          <td>
+                            <strong>{kat.jumlah.kadar_air}</strong>
+                          </td>
+                          <td>
+                            <strong>{kat.jumlah.kadar_kotoran}</strong>
+                          </td>
+                          <td>
+                            <strong>{kat.jumlah.do}</strong>
+                          </td>
+                          <td>
+                            <strong>{kat.jumlah.hi}</strong>
+                          </td>
+                        </tr>
+                      </>
+                    ))}
 
-                      {/* Baris jumlah untuk kategori */}
-                      <tr
-                        key={`jumlah-${index}-${katIndex}`}
-                        className="table-secondary"
-                      >
-                        <td>
-                          <strong>Total {kat.nama}</strong>
-                        </td>
-                        <td>{}</td>
-                        <td>
-                          <strong>{kat.jumlah.stok}</strong>
-                        </td>
-                        <td>
-                          <strong>{kat.jumlah.alb}</strong>
-                        </td>
-                        <td>
-                          <strong>{kat.jumlah.kadar_air}</strong>
-                        </td>
-                        <td>
-                          <strong>{kat.jumlah.kadar_kotoran}</strong>
-                        </td>
-                        <td>
-                          <strong>{kat.jumlah.do}</strong>
-                        </td>
-                        <td>
-                          <strong>{kat.jumlah.hi}</strong>
-                        </td>
-                      </tr>
-                    </>
-                  ))}
-                </>
-              ))}
-            </tbody>
-          </Table>
+                    {/* PKM dan Kernel diletakkan di bawah setelah kategori */}
+                    <tr key={`pkm-${index}`} className="table-warning">
+                      <td colSpan={2}>
+                        <strong>PKM</strong>
+                      </td>
+                      <td>{item.pkm.stok}</td>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                      <td>{item.pkm.nilai_do}</td>
+                      <td>{item.pkm.nilai_hi}</td>
+                    </tr>
+
+                    <tr key={`kernel-${index}`} className="table-warning">
+                      <td colSpan={2}>
+                        <strong>Kernel</strong>
+                      </td>
+                      <td>{item.kernel.stok}</td>
+                      <td>{item.kernel.alb}</td>
+                      <td>{item.kernel.kadar_air}</td>
+                      <td>{item.kernel.kadar_kotoran}</td>
+                      <td>{item.kernel.do}</td>
+                      <td>{item.kernel.hi}</td>
+                    </tr>
+                  </>
+                ))}
+              </tbody>
+            </Table>
           </div>
         </Col>
       </Row>
